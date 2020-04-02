2219f54970352d30ec0808dfa6eabcadb1c71f57
Crowdbotics <team@crowdbotics.com>
2020-04-02 03:31:24 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/skills_track_327_ca_15326/MainActivity.java
@@ -0,0 +1,15 @@
+package com.skills_track_327_ca_15326;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "skills_track_327_ca_15326";
+    }
+}
